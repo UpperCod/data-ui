78af18295ae8971e0bce449b11ca50f19f3bc219
UpperCod <mtrujillo@outlook.cl>
2020-08-05 11:04:24 -0400

    update build

--- docs/assets/1291-index.js
+++ docs/assets/1291-index.js
@@ -953,7 +953,7 @@ const DataIconArrow = ({
       }}`)
       , type == "arrow" && (
         h('svg', { height: size, viewBox: "0 0 13 9"   ,}
-          , h('g', { transform: transform,}
+          , h('g', null
             , h('path', {
               d: "M5.689,1.122a1,1,0,0,1,1.621,0l4.544,6.292A1,1,0,0,1,11.044,9H1.956a1,1,0,0,1-.811-1.585Z",
               fill: color,}
@@ -963,7 +963,7 @@ const DataIconArrow = ({
       )
       , type == "equal" && (
         h('svg', { width: "11.001", height: size, viewBox: "0 0 11.001 8"   ,}
-          , h('g', { transform: transform,}
+          , h('g', null
             , h('path', {
               d: "M-2073.6-1809a1.5,1.5,0,0,1-1.5-1.5,1.5,1.5,0,0,1,1.5-1.5h8a1.5,1.5,0,0,1,1.5,1.5,1.5,1.5,0,0,1-1.5,1.5Zm0-5a1.5,1.5,0,0,1-1.5-1.5,1.5,1.5,0,0,1,1.5-1.5h8a1.5,1.5,0,0,1,1.5,1.5,1.5,1.5,0,0,1-1.5,1.5Z",
               transform: "translate(2075.1 1817)" ,
@@ -1000,3 +1000,69 @@ DataIconArrow.props = {
 };
 
 customElements.define("data-icon", c(DataIconArrow));
+
+var n=function(t,s,r,e){var u;s[0]=0;for(var h=1;h<s.length;h++){var p=s[h++],a=s[h]?(s[0]|=p?1:2,r[s[h++]]):s[++h];3===p?e[0]=a:4===p?e[1]=Object.assign(e[1]||{},a):5===p?(e[1]=e[1]||{})[s[++h]]=a:6===p?e[1][s[++h]]+=a+"":p?(u=t.apply(a,n(t,a,r,["",null])),e.push(u),a[0]?s[0]|=2:(s[h-2]=0,s[h]=u)):e.push(a);}return e},t=new Map;function htm(s){var r=t.get(this);return r||(r=new Map,t.set(this,r)),(r=n(this,r.get(s)||(r.set(s,r=function(n){for(var t,s,r=1,e="",u="",h=[0],p=function(n){1===r&&(n||(e=e.replace(/^\s*\n\s*|\s*\n\s*$/g,"")))?h.push(0,n,e):3===r&&(n||e)?(h.push(3,n,e),r=2):2===r&&"..."===e&&n?h.push(4,n,0):2===r&&e&&!n?h.push(5,0,!0,e):r>=5&&((e||!n&&5===r)&&(h.push(r,0,e,s),r=6),n&&(h.push(r,n,0,s),r=6)),e="";},a=0;a<n.length;a++){a&&(1===r&&p(),p(a));for(var l=0;l<n[a].length;l++)t=n[a][l],1===r?"<"===t?(p(),h=[h],r=3):e+=t:4===r?"--"===e&&">"===t?(r=1,e=""):e=t+e[0]:u?t===u?u="":e+=t:'"'===t||"'"===t?u=t:">"===t?(p(),r=1):r&&("="===t?(r=5,s=e,e=""):"/"===t&&(r<5||">"===n[a][l+1])?(p(),3===r&&(h=h[0]),r=h,(h=h[0]).push(2,0,r),r=0):" "===t||"\t"===t||"\n"===t||"\r"===t?(p(),r=2):e+=t),3===r&&"!--"===e&&(r=4,h=h[0]);}return p(),h}(s)),r),arguments,[])).length>1?r:r[0]}
+
+const html = htm.bind(h);
+
+const DataIconArrow$1 = ({
+  type,
+  size,
+  color,
+  transform,
+  opacity,
+}) => {
+  return html`
+    <host shadowDom>
+      <style>
+        ${`g {opacity:${opacity}; transform-origin: center; transition:1s ease all; transform:${
+          transform || "none"
+        }}`}
+      </style>
+      ${type == "arrow" &&
+      html` <svg height="${size}" viewBox="0 0 13 9">
+        <g>
+          <path
+            d="M5.689,1.122a1,1,0,0,1,1.621,0l4.544,6.292A1,1,0,0,1,11.044,9H1.956a1,1,0,0,1-.811-1.585Z"
+            fill="${color}"
+          />
+        </g>
+      </svg>`}
+      ${type == "equal" &&
+      html`<svg width="11.001" height="${size}" viewBox="0 0 11.001 8">
+        <g>
+          <path
+            d="M-2073.6-1809a1.5,1.5,0,0,1-1.5-1.5,1.5,1.5,0,0,1,1.5-1.5h8a1.5,1.5,0,0,1,1.5,1.5,1.5,1.5,0,0,1-1.5,1.5Zm0-5a1.5,1.5,0,0,1-1.5-1.5,1.5,1.5,0,0,1,1.5-1.5h8a1.5,1.5,0,0,1,1.5,1.5,1.5,1.5,0,0,1-1.5,1.5Z"
+            transform="translate(2075.1 1817)"
+            fill="${color}"
+          />
+        </g>
+      </svg>`}
+    </host>
+  `;
+};
+
+DataIconArrow$1.props = {
+  type: {
+    type: String,
+    value: "arrow",
+  },
+  size: {
+    type: String,
+    value: "10",
+  },
+  color: {
+    type: String,
+    value: "#1FE668",
+  },
+  transform: {
+    type: String,
+    value: "",
+  },
+  opacity: {
+    type: Number,
+    value: 1,
+  },
+};
+
+customElements.define("data-icon-html", c(DataIconArrow$1));
